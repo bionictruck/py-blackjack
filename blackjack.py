29e17f7b6a5635abfb70a9a9a6d394052998cf9f
bionictruck <jaytrommer@charter.net>
2017-04-23 20:55:40 -0500

    Grammar and Ace fixes

--- blackjack.py
+++ blackjack.py
@@ -25,7 +25,7 @@ def bet():
         try:
             wager = Decimal(input("Place a bet between $5 and $" + str(wallet) + " $"))
         except Exception:
-            print("Invalid entry. Please enter a bet between $5 and $" + str(wallet))
+            print("Invalid entry. Please enter a bet between $5 and $" + str(wallet) + ".")
             time.sleep(2)
             bet()
         if wager < 5:
@@ -33,7 +33,7 @@ def bet():
             time.sleep(2)
             bet()
         elif wager > wallet:
-            print("You don't have enough money! Enter a bet less than $" + str(wallet))
+            print("You don't have enough money! Enter a bet less than $" + str(wallet) + ".")
             time.sleep(2)
             bet()
         else:
@@ -54,20 +54,65 @@ def play():
         dealer[0] = 11
     elif dealer[1] == 1:
         dealer[1] = 11
+    print("The dealer's upcard is " + str(dealer[0]) + ".")
     player.append(random.randrange(1, 11))
     player.append(random.randrange(1, 11))
-    print("The dealer's upcard is " + str(dealer[0]))
-    print("You have a total of " + str(sum(player)))
+    if player[0] == 1 and player[1] == 1:
+        player[0] = 11
+        player[1] = 1
+        print("You were dealt two Aces for a total of " + str(sum(player)) + ".")
+    elif player[0] == 1:
+        print("Your first card is an Ace and your second card is " + str(player[1]) + ".")
+        if sum(player) == 11:
+            print("You have 21. BLACKJACK!")
+            wallet += Decimal((wager * 3 / 2))
+            playagain()
+        else:
+            first_card()
+    elif player[1] == 1:
+        print("Your first card is " + str(player[0]) + " and your second card is an Ace.")
+        if sum(player) == 11:
+            print("You have 21. BLACKJACK!")
+            wallet += Decimal((wager * 3 / 2))
+            playagain()
+        else:
+            second_card()
+    else:
+        print("You have a total of " + str(sum(player)) + ".")
     if sum(dealer) == 21:
         print("The dealer has 21. BLACKJACK! Better luck next time.")
         wallet -= Decimal(wager)
         playagain()
-    elif sum(player) == 21:
-        print("You have 21. BLACKJACK!")
-        wallet += Decimal((wager * 3 / 2))
-        playagain()
     else:
         decision()
+
+def first_card():
+    ace = input("Would you like the Ace to be a 1 or 11? Type O or E. ")
+    if ace.lower() == "one" or ace.lower() == "o":
+        print("You have a total of " + str(sum(player)) + ".")
+        decision()
+    elif ace.lower() == "eleven" or ace.lower() == "e":
+        player[0] = 11
+        print("You have a total of " + str(sum(player)) + ".")
+        decision()
+    else:
+        print("Invalid entry. Please use O or E.")
+        time.sleep(1)
+        first_card()
+
+def second_card():
+    ace = input("Would you like the Ace to be a 1 or 11? Type O or E. ")
+    if ace.lower() == "one" or ace.lower() == "o":
+        print("You have a total of " + str(sum(player)) + ".")
+        decision()
+    elif ace.lower() == "eleven" or ace.lower() == "e":
+        player[1] = 11
+        print("You have a total of " + str(sum(player)) + ".")
+        decision()
+    else:
+        print("Invalid entry. Please use O or E.")
+        time.sleep(1)
+        second_card()
 
 def decision():
     global wallet
@@ -116,14 +161,14 @@ def hit():
     player.append(random.randrange(1, 11))
     if player[-1] == 1:
         aces()
-    print("You receive a " + str(player[-1]) + " and have a total of " + str(sum(player)))
+    print("You receive a " + str(player[-1]) + " and have a total of " + str(sum(player)) + ".")
     if sum(player) <= 21:
         print("The dealer's upcard is " + str(dealer[0]))
         choice = input("Would you like to (H)it or (S)tand? ")
         if choice.lower() == "h" or choice.lower() == "hit":
             hit()
         elif choice.lower() == "s" or choice.lower() == "stand":
-            print("You have chosen to stand")
+            print("You have chosen to stand.")
             computer()
     else:
         print("Sorry, you are over 21 and have busted.")
@@ -133,11 +178,11 @@ def hit():
 def aces():
     ace = input("You recieved an Ace. Would you like the Ace to be a 1 or 11? Type O or E. ")
     if ace.lower() == "one" or ace.lower() == "o":
-        print("You have a total of " + str(sum(player)))
+        print("You have a total of " + str(sum(player)) + ".")
         decision()
     elif ace.lower() == "eleven" or ace.lower() == "e":
         player[-1] = 11
-        print("You have a total of " + str(sum(player)))
+        print("You have a total of " + str(sum(player)) + ".")
         decision()
     else:
         print("Invalid entry. Please use O or E.")
@@ -147,7 +192,7 @@ def aces():
 def computer():
     global wallet
     global wager
-    print("The dealer has a total of " + str(sum(dealer)))
+    print("The dealer has a total of " + str(sum(dealer)) + ".")
     time.sleep(2)
     if sum(dealer) < 17:
         dealer.append(random.randrange(1, 11))
@@ -155,18 +200,18 @@ def computer():
             print("The dealer takes a hit and receives an Ace.")
             if (sum(dealer) + 10) <= 21:
                 dealer[-1] = 11
-                ("The dealer now has " + str(sum(dealer)))
+                ("The dealer now has " + str(sum(dealer)) + ".")
                 computer()
             else:
-                ("The dealer now has " + str(sum(dealer)))
+                ("The dealer now has " + str(sum(dealer)) + ".")
                 computer()
         else:
-            print("The dealer takes a hit receives a " + str(dealer[-1]))
+            print("The dealer takes a hit receives a " + str(dealer[-1]) + ".")
             time.sleep(1)
             if sum(dealer) < 17:
                 computer()
             elif sum(dealer) >= 17 and sum(dealer) <= 21:
-                print("The dealer stands with " + str(sum(dealer)))
+                print("The dealer stands with " + str(sum(dealer)) + ".")
                 time.sleep(1)
                 result()
             else:
@@ -174,15 +219,15 @@ def computer():
                 wallet += wager
                 playagain()
     elif sum(dealer) >= 17:
-        print("The dealer stands with " + str(sum(dealer)))
+        print("The dealer stands with " + str(sum(dealer)) + ".")
         time.sleep(1)
         result()
 
 def result():
     global wallet
     global wager
-    print("The dealer has " + str(sum(dealer)))
-    print("You have " + str(sum(player)))
+    print("The dealer has " + str(sum(dealer)) + ".")
+    print("You have " + str(sum(player)) + ".")
     if sum(dealer) > sum(player):
         print("The dealer has won. Better luck next time.")
         wallet -= wager
@@ -190,7 +235,7 @@ def result():
     elif sum(dealer) < sum(player):
         print("You have won!")
         wallet += wager
-        print("You now have $" + str(wallet))
+        print("You now have $" + str(wallet) + ".")
         playagain()
     else:
         print("This hand is a push.")
@@ -199,12 +244,15 @@ def result():
 def playagain():
     global wallet
     global wager
-    replay = input("Would you like to play again? Y/N ")
+    replay = input("Would you like to play again? Y/N? ")
     if replay.lower() == 'y' or replay.lower() == 'yes':
         bet()
-    else:
-        print("You finished with $" + str(wallet))
+    elif replay.lower() == 'n' or replay.lower() == 'no':
+        print("You finished with $" + str(wallet) + ".")
         print("See you next time!")
         quit()
+    else:
+        print("Invalid entry. Please user Y or N.")
+        playagain()
 
 bet()
